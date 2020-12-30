be6f1438903b5002883bd1edc4ee9e5dbf4950d6
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-30 11:06:29 -0300

    add_ngMOdel_ngForm_ngSubmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { DataFormComponent } from './data-form/data-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
