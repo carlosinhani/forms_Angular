e6862b2d29cd07aa87849d5e036bece90cdb6967
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-30 11:28:42 -0300

    Forms_templete-driven_two-way_data-binding

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Rodrigo',
+    email: 'carlos.inhani@gmail.com'
+  }
+
 
   onSubmit(form){
-    console.log(form);
+    console.log(form.value);
+
+    console.log(this.usuario);
   }
 
   constructor() { }
