46cf13cd8db12c2637de83493e52f7492f2cd3a2
Carlos.Inhani <carlos.inhani@gmail.com>
2021-01-04 15:23:52 -0300

    template_json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { TemplateFormModule } from './template-form/template-form.module';
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
+
   ],
   imports: [
     BrowserModule,
