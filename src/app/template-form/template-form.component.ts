f87053a107350e1407bcf601ee2258c787a4a1ac
Carlos.Inhani <carlos.inhani@gmail.com>
2021-01-05 10:59:27 -0300

    endereço_automatico_com_cep

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'template-form',
@@ -19,7 +22,7 @@ export class TemplateFormComponent implements OnInit {
     // console.log(this.usuario);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
@@ -34,5 +37,25 @@ export class TemplateFormComponent implements OnInit {
       'has-feedback': this.verificaValidTouched(campo)
     }
   }
+  consultaCEP(cep){
+    //Nova variável "cep" somente com dígitos.
+     cep = cep.replace(/\D/g, '');
 
+    //Verifica se campo cep possui valor informado.
+      if (cep != "") {
+
+       //Expressão regular para validar o CEP.
+       var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+        .map(dados => dados.json())
+        .subscribe(dados => console.log(dados));
+      }
+
+
+    }
+  }
 }
