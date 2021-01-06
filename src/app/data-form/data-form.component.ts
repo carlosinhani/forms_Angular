27e3b0eb57d86da9a2636b9fdb8e323ae28bcec9
Carlos.Inhani <carlos.inhani@gmail.com>
2021-01-06 11:29:18 -0300

    validacao_campos

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'data-form',
@@ -24,8 +29,8 @@ export class DataFormComponent implements OnInit {
     // });
 
     this.formulario = this.formBuilder.group({
-        nome: [null],
-        email: [null]
+        nome: [null, Validators.required],
+        email: [null, [Validators.required, Validators.email]]
     });
   }
   onSubmit(){
@@ -37,7 +42,7 @@ export class DataFormComponent implements OnInit {
 
         //reseta o form
         // this.formulario.reset();
-        this.resetar();
+        // this.resetar();
       },
       (error: any) => alert('erro'));
   }
